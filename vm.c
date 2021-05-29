25eaa2110a8a5f59370b6490a85092da818c7c50
galacticgoob <65988711+galacticgoob@users.noreply.github.com>
2021-05-28 21:48:18 -0700

    updated copyuvm

--- vm.c
+++ vm.c
@@ -323,8 +323,7 @@ copyuvm(pde_t *pgdir, uint sz)
 
   if((d = setupkvm()) == 0)
     return 0;
-  //for(i = 0; i < sz; i += PGSIZE){
-  for(i = stp; i > stp - (curproc->stackAmount) * PGSIZE; i -= PGSIZE){ // not sure
+  for(i = 0; i < sz; i += PGSIZE){
     if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
       panic("copyuvm: pte should exist");
     if(!(*pte & PTE_P))
@@ -337,6 +336,21 @@ copyuvm(pde_t *pgdir, uint sz)
     if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
       goto bad;
   }
+
+  for(i = stp; i > stp - (curproc->stackAmount) * PGSIZE; i -= PGSIZE){ // need to copy stack and page guard
+      if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+          panic("copyuvm: pte should exist");
+      if(!(*pte & PTE_P))
+          panic("copyuvm: page not present");
+      pa = PTE_ADDR(*pte);
+      flags = PTE_FLAGS(*pte);
+      if((mem = kalloc()) == 0)
+          goto bad;
+      memmove(mem, (char*)P2V(pa), PGSIZE);
+      if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
+          goto bad;
+    }
+
   return d;
 
 bad:
